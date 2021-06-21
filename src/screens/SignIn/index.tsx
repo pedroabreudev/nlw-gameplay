69ce8917e5584f2f6757ea0512ae1da480ae368a
Pedro Henrique <pedroabreu.dev@gmail.com>
2021-06-21 16:53:22 -0400

    Finalizando a Aula 01 = #together

--- src/screens/SignIn/index.tsx
+++ src/screens/SignIn/index.tsx
@@ -1,17 +1,52 @@
-import React, {useState} from 'react';
-import { View, Text, TextInput} from 'react-native';
+import React, { } from 'react';
+import { 
+  View,
+  Text,
+  Image,
+  StatusBar
+  
+} from 'react-native';
 
+import{ ButtonIcon } from '../../components/ButtomIcon';
+import IllustrationImg from '../../assets/illustration.png';
 import { styles } from './styles';
 
 
 export function SignIn(){
-  const [] = useState('');
-  
   return (
-    <View style = {styles.container}> 
-      <Text> Hello World, NLW Together </Text>
-      <TextInput style={styles.input}/>
+    <View style = {styles.container}>
+      <StatusBar
+        barStyle="light-content"
+        backgroundColor="transparent"
+        translucent
+      
+      />
 
+      <Image 
+        source={IllustrationImg}
+        style={styles.image}
+        resizeMode= "stretch"
+      />
+
+      <View style={styles.content}>
+        <Text style={styles.title}>
+          Organize {`\n`}
+          suas jogatinas {`\n`}
+          facilmente
+        </Text>
+
+        <Text style={styles.subtitle}>
+          Crie grupos para jogar seus games {`\n`}
+          favoritos com seus amigos
+
+        </Text>
+        <ButtonIcon 
+          title="Entrar com Discord"
+          activeOpacity={0.7}
+          
+        />
+
+      </View>      
     </View>
   );
 
